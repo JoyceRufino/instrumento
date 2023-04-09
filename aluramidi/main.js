521a1c8b7d17bf96de0a307a940e0f38623bbd95
joyce <joyce_rufino95@hotmail.com>
2023-04-09 11:27:07 -0300

    repetição otimizada com for

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -7,23 +7,18 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 //condição
 let contador = 0;
 
-//enquanto
-while (contador < listaDeTeclas.length) {
+
+for (let contador =0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
 
     const instrumento = tecla.classList[1];
 
-    // template string
     const idAudio = `#som_${instrumento}`;
-
-    //console.log (idAudio);
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    contador = contador + 1;
-
-    //console.log (contador);
+    
 }
